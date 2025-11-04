1539a0c3eb4f70e705a180d285d3395191675ad1
Avelanda <gorthell@gmail.com>
2025-11-04 16:33:07 +0200

    Update types.ts
    Modification of a granular model into a modular function.

--- src/setup/platformSetup/types.ts
+++ src/setup/platformSetup/types.ts
@@ -1,6 +1,23 @@
+/*
+ * Copyright © 2025 Avelanda.
+ * All rights reserved.
+ */
+
 type PlatformSpecificUpdater = {
     update: () => void;
     init?: () => void;
 };
+
+function CorePlatformSU(PlatformSpecificUpdater): number|string{
+
+ update: (true||false);
+ if (!false){
+  return 0;
+ } 
+ while (CorePlatformSU && PlatformSpecificUpdater){
+  return 0;
+ }
+    
+}
 
 export default PlatformSpecificUpdater;
